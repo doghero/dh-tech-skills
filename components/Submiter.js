fd30403a41b11306344e8e72efbd3166a45713a6
FernandoMedin <fernando-medin@outlook.com>
2019-07-02 02:18:17 -0300

    Submiter

--- components/Submiter.js
+++ components/Submiter.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
 type Props = {}
 
+const api = 'https://us-central1-dh-backend.cloudfunctions.net/performaceReview';
+
 class Submiter extends React.Component {
 	submitPerformace = () => {
-		alert("Submit");
+		var params = window.location.href.split("/#").pop();
+		var review = document.getElementById("review").value;
+		location.href = api + "?params=" + params + "&review=" + review;
 	}
 	
 	render = () => {
-		return <input type="button" id="submitPerformace" name="submitPerformace" value="Submit performace review" onClick={this.submitPerformace} />
+		return (
+			<div>
+				<textarea id="review" cols="100" rows="33">
+					Digite
+				</textarea>
+				<br /><br/>
+				<input type="button" id="submitPerformace" name="submitPerformace" value="Submit performace review" onClick={this.submitPerformace} />
+			</div>
+		)
 	}
 }
 
