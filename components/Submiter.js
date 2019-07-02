e257c1dd962f254897f17dd016a9ea5ece976e80
FernandoMedin <fernando-medin@outlook.com>
2019-07-02 07:33:56 -0300

    request via axios to local api

--- components/Submiter.js
+++ components/Submiter.js
@@ -3,20 +3,24 @@ import axios from 'axios';
 
 type Props = {}
 
-const api = 'https://us-central1-dh-backend.cloudfunctions.net/performaceReview';
+const api = 'http://127.0.0.1:5000/review/create';
 
 class Submiter extends React.Component {
 	submitPerformace = () => {
-		var params = window.location.href.split("/#").pop();
-		var review = document.getElementById("review").value;
-		location.href = api + "?params=" + params + "&review=" + review;
+		var data = {
+			params: window.location.href.split("/#").pop(),
+			review: document.getElementById("review").value
+		}
+	
+		axios.post(api, data).then(res => {
+			alert(res.data);
+		});
 	}
 	
 	render = () => {
 		return (
 			<div>
-				<textarea id="review" cols="100" rows="33">
-					Digite
+				<textarea id="review" cols="100" rows="33" placeholder="Para seu review siga essas regras:&#10;1- Auto avaliação ou gestor direto: descrever cada nota.&#10;2- Outros ou gestor indireto: descrever cada quadrante apenas.&#10;Por exemplo:&#10;Building: Essas notas por conta disso.&#10;Building Android: Essa nota por conta disso.">
 				</textarea>
 				<br /><br/>
 				<input type="button" id="submitPerformace" name="submitPerformace" value="Submit performace review" onClick={this.submitPerformace} />
